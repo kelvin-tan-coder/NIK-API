0b3477e60af642c1958190dd555973032acfa5f9
kelvin-tan-coder <kelvin@devhaus.com.sg>
2023-12-15 09:36:46 +0700

    v1.0

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const url = 'https://indonesian-identification-card-ktp.p.rapidapi.com/api/v3/check?nik=5171041903640005';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '58de2fc459msh581b4ca6cac3383p157663jsnae729f0ea479',
-		'X-RapidAPI-Host': 'indonesian-identification-card-ktp.p.rapidapi.com'
-	}
-};
+// const searchButton = document.querySelector("#search");
+// const userInput = document.querySelector("#masuk").value;
 
+async function checkNIK() {
+  const url =
+    "https://indonesian-identification-card-ktp.p.rapidapi.com/api/v3/check?nik=5171041903640005";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "58de2fc459msh581b4ca6cac3383p157663jsnae729f0ea479",
+      "X-RapidAPI-Host": "indonesian-identification-card-ktp.p.rapidapi.com",
+    },
+  };
 
-async function checkNIK(){
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-};
 
 checkNIK();
